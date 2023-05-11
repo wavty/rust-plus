28d3d8f0f012cabb592222defa7cdbc4b599f204
wavty <mtaobo@gmail.com>
2023-05-11 22:00:21 +0800

    feat: httpie output add colored support
    https://docs.rs/colored/latest/colored/trait.Colorize.html

--- gethandsdirty/httpie/src/main.rs
+++ gethandsdirty/httpie/src/main.rs
@@ -4,7 +4,9 @@ use std::collections::HashMap;
 use anyhow::{Ok, Result};
 use clap::Parser;
 use cmd::{Get, Post, Subcmd};
-use reqwest::Client;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::{header, Client, Response};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,18 +20,53 @@ async fn main() -> Result<()> {
     Ok(result)
 }
 
-async fn get(client: Client, args: &Get) -> Result<()> {
-    let resp = client.get(&args.url).send().await?;
-    println!("{:?}", resp.text().await?);
-    Ok(())
-}
-
 async fn post(client: Client, args: &Post) -> Result<()> {
     let mut body = HashMap::new();
     for pair in args.body.iter() {
         body.insert(&pair.key, &pair.value);
     }
     let resp = client.post(&args.url).json(&body).send().await?;
-    println!("{:?}", resp.text().await?);
+    Ok(print_resp(resp).await?)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers().get(header::CONTENT_TYPE).map(|v| {
+        let tmp = v.to_str().unwrap().parse();
+        tmp.unwrap()
+    })
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
     Ok(())
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status)
+}
+
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    print!("\n")
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        _ => println!("{}", body),
+    }
 }
