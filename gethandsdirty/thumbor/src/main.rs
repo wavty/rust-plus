bbfbeac3c7e06699f9fd91fd8f934c86a5b1ff3c
wavty <mtaobo@gmail.com>
2023-05-18 23:22:08 +0800

    feat: add test function

--- gethandsdirty/thumbor/src/main.rs
+++ gethandsdirty/thumbor/src/main.rs
@@ -1,9 +1,19 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    num::NonZeroUsize,
+    sync::{Arc, Mutex},
+};
 
 use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use bytes::Bytes;
+use lru::LruCache;
 use pb::ImageSpec;
-use percent_encoding::percent_decode_str;
+use percent_encoding::{percent_decode_str, percent_encode, NON_ALPHANUMERIC};
 use serde::Deserialize;
+use tower::ServiceBuilder;
+use tower_http::add_extension::AddExtensionLayer;
+
+use crate::pb::{filter, resize, Spec};
 mod pb;
 
 // 测试代码
@@ -16,15 +26,25 @@ struct Params {
     url: String,
 }
 
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(NonZeroUsize::new(1024).unwrap())));
     // 定义路由
-    let app = Router::new().route("/image/:spec/:url", get(generate));
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(cache))
+                .into_inner(),
+        );
     // 运行web服务器
     let addr: SocketAddr = "127.0.0.1:3000".parse().unwrap();
     tracing::info!("listening on http://{}", addr);
+    print_test_url("https://img-blog.csdnimg.cn/7ab510402f3e4f8295d76764a3639eed.png");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -37,5 +57,16 @@ async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, St
         .as_str()
         .try_into()
         .map_err(|_| StatusCode::BAD_REQUEST)?;
-    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+    Ok(format!("url: {},\nspec: {:#?}", url, spec))
+}
+
+fn print_test_url(url: &str) {
+    use std::borrow::Borrow;
+    let spec1 = Spec::new_resize(500, 800, resize::SampleFilter::CatmullRom);
+    let spec2 = Spec::new_watermark(20, 20);
+    let spec3 = Spec::new_filter(filter::Filter::Marine);
+    let image_spec = ImageSpec::new(vec![spec1, spec2, spec3]);
+    let s: String = image_spec.borrow().into();
+    let test_image = percent_encode(url.as_bytes(), NON_ALPHANUMERIC).to_string();
+    tracing::info!("test url: http://127.0.0.1:3000/image/{}/{}", s, test_image);
 }
